a8da7cc3bacc89217167700e9dda44f58828acba
xjdeng <xjd001@gmail.com>
2019-02-17 16:45:41 -0600

    Scrape URL to download card image

--- downloader.py
+++ downloader.py
@@ -9,3 +9,6 @@ class Card(object):
         res.raise_for_status()
         self.soup = bs4.BeautifulSoup(res.text, 'lxml')
         self.title = self.soup.select('div[class="contentTitle"]')[0].text.strip()
+        self.imgurl = "http://gatherer.wizards.com/" + \
+        self.soup.select('img[id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_cardImage"]')[0].get('src').strip("../../")
+        
